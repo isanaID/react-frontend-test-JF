df70ccfc7f62af4484fad98b832eee448243ed94
isanaid <eizisana@gmail.com>
2022-05-12 12:48:12 +0700

    delete waste

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import * as React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux';
+// import { getUser } from './api/user';
+import store from './app/store';
+import { listener } from './app/listener';
 
 function App() {
+  React.useEffect(() => {
+    listener();
+  }, []);
+
   return (
-    <div className="App">
+    <div>
+      <Provider store={store}>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
       </Router>
-
+      </Provider>
     </div>
   );
 }
